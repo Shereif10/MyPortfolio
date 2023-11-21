7893de41c8376313bbc7797897da5e1a4d4c9890
Shereif10 <shereifelswaf7@gmail.com>
2023-11-21 16:58:48 +0200

    seven

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -102,7 +102,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./public/desktop_pc/scene.glb");
+  const computer = useGLTF("/desktop_pc/scene.glb");
 
   return (
     <mesh>
