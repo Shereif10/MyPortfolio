d49a1fc90855305453dec5492c5cecbe39cae34a
Shereif10 <shereifelswaf7@gmail.com>
2023-11-21 02:31:33 +0200

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,460 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    github,
+    nextjs,
+    regex,
+    bootstrap,
+    devfolio,
+    fokir,
+    daniels,
+    smart,
+    crud,
+    weather,
+    yummy,
+    start,
+    bookmark
+  } from "../assets/assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Framework",
+      icon: mobile,
+    },
+    {
+      title: "Responsive Web Apps",
+      icon: backend,
+    },
+    // {
+    //   title: "Problem Solving",
+    //   icon: creator,
+    // },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    // {
+    //   name: "Tailwind CSS",
+    //   icon: tailwind,
+    // },
+    // {
+    //   name: "Node JS",
+    //   icon: nodejs,
+    // },
+    // {
+    //   name: "MongoDB",
+    //   icon: mongodb,
+    // },
+    // {
+    //   name: "Three JS",
+    //   icon: threejs,
+    // },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "GitHub",
+      icon: github,
+    },
+    {
+      name: "Next.js",
+      icon: nextjs,
+    },
+    {
+      name: "RegEx",
+      icon: regex,
+    },
+    {
+      name: "Bootstrap",
+      icon: bootstrap,
+    }
+    // {
+    //   name: "docker",
+    //   icon: docker,
+    // },
+  ];
+  
+  const experiences = [
+    {
+      title: "React.js Developer",
+      company_name: "Starbucks",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "March 2020 - April 2021",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "React Native Developer",
+      company_name: "Tesla",
+      icon: tesla,
+      iconBg: "#E6DEDD",
+      date: "Jan 2021 - Feb 2022",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Web Developer",
+      company_name: "Shopify",
+      icon: shopify,
+      iconBg: "#383E56",
+      date: "Jan 2022 - Jan 2023",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Full stack Developer",
+      company_name: "Meta",
+      icon: meta,
+      iconBg: "#E6DEDD",
+      date: "Jan 2023 - Present",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Fokir",
+      description:
+        "Fokir template shows my skills in using HTML and CSS , and using those skills to create a responsive websites and a beautiful user interface.",
+      tags: [
+        {
+          name: "HTML",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "green-text-gradient",
+        },
+        // {
+        //   name: "scss",
+        //   color: "pink-text-gradient",
+        // },
+      ],
+      image: fokir,
+      source_code_link: "https://github.com/Shereif10/Fokir",
+    },
+    {
+      name: "DevFolio",
+      description:
+        "A DavFolio template explaining Morgan Freeman's work. The template also shows an overview of Morgan's skills, services , projects, customer feedback and a contact page.",
+      tags: [
+        {
+          name: "HTML",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Bootstrap",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: devfolio,
+      source_code_link: "https://github.com/Shereif10/DevFolio",
+    },
+    {
+      name: "Daniels",
+      description:
+        "Daniel's template was an exam in the form of a portfolio website to use what I learned in HTML, CSS, and Bootstrab to create a responsive website..",
+      tags: [
+        {
+          name: "HTML",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Bootstrap",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: daniels,
+      source_code_link: "https://github.com/Shereif10/Daniel",
+    },
+    {
+      name: "Smart Login",
+      description:
+        "Smart login is a thumbnail of the authentication page. You can create an account by username, email and password. You can also login or logout from the website.",
+      tags: [
+        {
+          name: "HTML",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Bootstrap",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "JS",
+          color: "blue-text-gradient",
+        },
+      ],
+      image: smart,
+      source_code_link: "https://github.com/Shereif10/Smart-Login",
+    },
+    {
+      name: "CRUD",
+      description:
+        "CRUD system makes you add product by product name, price, category and description. It also display all of products in a list so you can search, update and delete.",
+      tags: [
+        {
+          name: "HTML",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Bootstrap",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "JS",
+          color: "blue-text-gradient",
+        },
+      ],
+      image: crud,
+      source_code_link: "https://github.com/Shereif10/CRUD",
+    },
+    {
+      name: "BookMark",
+      description:
+        "Bookmark is a website that allows you add the book name and the book URL and display it in a list. So you can delete any item or visit the book site. ",
+      tags: [
+        {
+          name: "HTML",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Bootstrap",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "JS",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "SweetAlert",
+          color: "green-text-gradient",
+        },
+      ],
+      image: bookmark,
+      source_code_link: "https://github.com/Shereif10/BookMark",
+    },
+    {
+      name: "Weather",
+      description:
+        "Weather app knows your location and display the temperatures for the next 3 days. You can also search for temperatures in any city in the world.",
+      tags: [
+        {
+          name: "HTML",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Bootstrap",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "JS",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "API",
+          color: "green-text-gradient",
+        },
+      ],
+      image: weather,
+      source_code_link: "https://github.com/Shereif10/weather",
+    },
+    {
+      name: "Yummy",
+      description:
+        "yummy is a website that displays popular meals in various countries of the world.  When you click on any meal, the meal details are displayed.You can also search for any meal.",
+      tags: [
+        {
+          name: "HTML",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Bootstrap",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "JS",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "API",
+          color: "green-text-gradient",
+        },
+      ],
+      image: yummy,
+      source_code_link: "https://github.com/Shereif10/Yummy",
+    },
+    {
+      name: "Start Framework",
+      description:
+        "Start framework is a simple website to start the React journey. It contains more than one page. You can navigate through each page using router.",
+      tags: [
+        {
+          name: "HTML",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "CSS",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Bootstrap",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "JS",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "React",
+          color: "green-text-gradient",
+        },
+      ],
+      image: start,
+      source_code_link: "https://github.com/Shereif10/start-framework",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
